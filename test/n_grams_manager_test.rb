e821f123e79ad1141640db9901bb3e16c5e417ce
Mariusz Kielan <mariusz@kielan.org>
2013-12-31 18:09:47 +0100

    Drobna zmiana w NGramsManagerTest

--- test/n_grams_manager_test.rb
+++ test/n_grams_manager_test.rb
@@ -4,15 +4,15 @@ require_relative '../test/test_text_mining_helper'
 
 include TextMining
 
-class NGramManagerTest < Test::Unit::TestCase
+class NGramsManagerTest < Test::Unit::TestCase
 
   # Called before every test method runs. Can be used
   # to set up fixture information.
   def setup
-    prepare_test_results_dir
+    prepare_test_results_dir NGramsManagerTest
 
-    @manager = TextMining::NGramsManager.new
-    @src = TextMining::Attachments::SheetSource.new '../data/EKG_opis.ods', header = 1
+    @manager = NGramsManager.new
+    @src = Attachments::SheetSource.new '../data/EKG_opis.ods', header = 1
 
     doc_id = 1
     while doc = @src.next[0].remove_punctuation!
